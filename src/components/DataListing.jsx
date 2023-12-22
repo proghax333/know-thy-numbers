3ac0c92142fca65a5d02c56cf97e671745238274
proghax333 <skillzworldtech@gmail.com>
2023-12-23 00:44:22 +0530

    changed page size

--- src/components/DataListing.jsx
+++ src/components/DataListing.jsx
@@ -1,41 +1,41 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import UpdateForm from "./UpdateForm"
-import env from "../secrets.js"
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import UpdateForm from "./UpdateForm";
+import env from "../secrets.js";
+import { useNavigate } from "react-router-dom";
 
-const { VITE_BACKEND_HOST } = env
+const { VITE_BACKEND_HOST } = env;
 
 const DataListing = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const [dataList, setDataList] = useState([]);
   const [selectedItem, setSelectedItem] = useState(null);
   const [isUpdating, setIsUpdating] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [total, setTotal] = useState(0)
+  const [total, setTotal] = useState(0);
   const [goToPage, setGoToPage] = useState(1);
-  const pageSize = 1
+  const pageSize = 10;
 
   useEffect(() => {
     // Fetch data from the backend using Axios
     const fetchData = async () => {
       try {
-        const url = `${VITE_BACKEND_HOST}/openapi/prediction/findall`
+        const url = `${VITE_BACKEND_HOST}/openapi/prediction/findall`;
         let response = await axios({
           method: "post",
           url,
           data: { currentPage, pageSize },
           headers: { "Content-Type": "application/json" },
           // withCredentials: true
-        })
+        });
 
-        console.log(response.data)
+        console.log(response.data);
 
         setDataList(response.data.data); // Assuming the response is an array of data
-        setTotal(response.data.totalPage)
+        setTotal(response.data.totalPage);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
 
@@ -79,7 +79,7 @@ const DataListing = () => {
   const handleBack = () => {
     setSelectedItem(null);
     setIsUpdating(false);
-    navigate("/listing")
+    navigate("/listing");
   };
 
   const handleNextPage = () => {
@@ -90,73 +90,75 @@ const DataListing = () => {
     setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
   };
 
-//   return (
-//     <div className="min-h-screen bg-gray-100 p-8">
-//       {selectedItem ? (
-//         <div className="bg-white p-8 shadow-md rounded-md w-full md:w-2/3 lg:w-1/2 xl:w-1/3 mx-auto">
-//           <h2 className="text-2xl font-bold mb-4 text-center">{isUpdating ? 'Update Data' : 'View Data'}</h2>
-//           {isUpdating && (
-//             <div className="mb-4">
-//               {/* Render your update form fields here */}
-//               {/* Example: */}
-//               {/* <input type="text" value={selectedItem.fieldName} onChange={(e) => handleFieldChange(e)} /> */}
-//             </div>
-//           )}
-//           {Object.entries(selectedItem).map(([key, value]) => (
-//             <div key={key} className="mb-4">
-//               <strong className="mr-2">{key}:</strong>
-//               <span>{value}</span>
-//             </div>
-//           ))}
-//           <div className="flex justify-end mt-4">
-//             <button
-//               type="button"
-//               onClick={handleBack}
-//               className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-//             >
-//               Back
-//             </button>
-//           </div>
-//         </div>
-//       ) : (
-//         <div>
-//           <h2 className="text-2xl font-bold mb-4 text-center">Data Listing</h2>
-//           {dataList.map((item) => (
-//             <div key={item.id} className="bg-white p-4 shadow-md rounded-md mb-4 flex items-center justify-between">
-//               <div>
-//                 <strong className="mr-2">ID:</strong>
-//                 <span>{item.id}</span>
-//               </div>
-//               <div>
-//                 <button
-//                   type="button"
-//                   onClick={() => handleView(item)}
-//                   className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2 focus:outline-none focus:shadow-outline"
-//                 >
-//                   View
-//                 </button>
-//                 <button
-//                   type="button"
-//                   onClick={() => handleUpdate(item)}
-//                   className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-//                 >
-//                   Update
-//                 </button>
-//               </div>
-//             </div>
-//           ))}
-//         </div>
-//       )}
-//     </div>
-//   );
-  console.log(dataList)
+  //   return (
+  //     <div className="min-h-screen bg-gray-100 p-8">
+  //       {selectedItem ? (
+  //         <div className="bg-white p-8 shadow-md rounded-md w-full md:w-2/3 lg:w-1/2 xl:w-1/3 mx-auto">
+  //           <h2 className="text-2xl font-bold mb-4 text-center">{isUpdating ? 'Update Data' : 'View Data'}</h2>
+  //           {isUpdating && (
+  //             <div className="mb-4">
+  //               {/* Render your update form fields here */}
+  //               {/* Example: */}
+  //               {/* <input type="text" value={selectedItem.fieldName} onChange={(e) => handleFieldChange(e)} /> */}
+  //             </div>
+  //           )}
+  //           {Object.entries(selectedItem).map(([key, value]) => (
+  //             <div key={key} className="mb-4">
+  //               <strong className="mr-2">{key}:</strong>
+  //               <span>{value}</span>
+  //             </div>
+  //           ))}
+  //           <div className="flex justify-end mt-4">
+  //             <button
+  //               type="button"
+  //               onClick={handleBack}
+  //               className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+  //             >
+  //               Back
+  //             </button>
+  //           </div>
+  //         </div>
+  //       ) : (
+  //         <div>
+  //           <h2 className="text-2xl font-bold mb-4 text-center">Data Listing</h2>
+  //           {dataList.map((item) => (
+  //             <div key={item.id} className="bg-white p-4 shadow-md rounded-md mb-4 flex items-center justify-between">
+  //               <div>
+  //                 <strong className="mr-2">ID:</strong>
+  //                 <span>{item.id}</span>
+  //               </div>
+  //               <div>
+  //                 <button
+  //                   type="button"
+  //                   onClick={() => handleView(item)}
+  //                   className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2 focus:outline-none focus:shadow-outline"
+  //                 >
+  //                   View
+  //                 </button>
+  //                 <button
+  //                   type="button"
+  //                   onClick={() => handleUpdate(item)}
+  //                   className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+  //                 >
+  //                   Update
+  //                 </button>
+  //               </div>
+  //             </div>
+  //           ))}
+  //         </div>
+  //       )}
+  //     </div>
+  //   );
+  console.log(dataList);
 
   const handleGoToUpload = () => {
-    navigate("/upload_data")
-  }
+    navigate("/upload_data");
+  };
 
   const handleGoToPage = () => {
-    setCurrentPage(() => Math.max(1, Math.min(goToPage, Math.ceil(total / pageSize))));
+    setCurrentPage(() =>
+      Math.max(1, Math.min(goToPage, Math.ceil(total / pageSize)))
+    );
   };
 
   return (
@@ -204,10 +206,15 @@ const DataListing = () => {
           </div>
           <h2 className="text-2xl font-bold mb-4 text-center">Data Listing</h2>
           {dataList.map((item) => (
-            <div key={item.id} className="bg-white p-4 shadow-md rounded-md mb-4 flex items-center justify-between">
+            <div
+              key={item.id}
+              className="bg-white p-4 shadow-md rounded-md mb-4 flex items-center justify-between"
+            >
               <div>
                 {/* <strong className="mr-2">ID:</strong> */}
-                <span>{item.id}&gt; {item.name}</span>
+                <span>
+                  {item.id}&gt; {item.name}
+                </span>
               </div>
               <div>
                 <button
@@ -267,7 +274,6 @@ const DataListing = () => {
       )}
     </div>
   );
-
 };
 
 export default DataListing;
